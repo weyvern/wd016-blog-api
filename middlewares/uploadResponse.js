c97ec39ff8100ab8bf7deb1ed8ec4e7e7951753c
Jorge Paul <jserranopaul@gmail.com>
2021-08-04 14:44:45 +0200

    Fixed multer error handling

--- middlewares/uploadResponse.js
+++ middlewares/uploadResponse.js
@@ -4,7 +4,7 @@ const uploadResponse = (req, res) => {
   const { file, fileValidationError, protocol } = req;
   const host = req.get('host');
   if (!file) throw new ErrorResponse('Please upload one picture', 400);
-  if (fileValidationError) throw new ErrorResponse(fileValidationError);
+  if (fileValidationError) throw new ErrorResponse(fileValidationError, 400);
   res.status(201).json({
     location: `${protocol}://${host}/uploads/${file.filename}`
   });
--- utils/ErrorResponse.js
+++ utils/ErrorResponse.js
@@ -1,7 +1,7 @@
 class ErrorResponse extends Error {
   constructor(message, code) {
     super(message);
-    this.code = code;
+    this.statusCode = code;
   }
 }
 
